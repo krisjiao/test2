aca8ebcfc3eac6e34261f515b9f3e84074e5322b
kriswu <2394278251@qq.com>
2020-11-05 00:40:28 +0800

    小红改代码了

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("HI!");
+        System.out.println("haole!");
     }
 }
